a7dc9a5bebbc28bb743ee561e3219a75d19f3863
Marcos Cantarutti <marcossandim21@gmail.com>
2021-10-23 11:09:40 -0300

    Versao desktop finalizada

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
 
 
 const linksSocialMedia = {
-    github: 'MarcosCantarutti',
+    github: 'marcosCantarutti',
     youtube: 'rocketseat',
     facebook: 'marcos.cantarutti' ,
     instagram: '',
     twitter: 'LakaNegro21'
 }
+
+
 
 function changeSocialMediaLinks() {
 
@@ -20,3 +22,23 @@ function changeSocialMediaLinks() {
 
 }
 changeSocialMediaLinks()
+
+
+const getGitHubProfileInfos = () => {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+    console.log(url)
+
+    fetch(url)
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userGithub.children[1].textContent = data.login
+        userGithub.href = data.html_url
+        userAvatar.children[1].src = data.avatar_url
+    })
+
+}
+
+getGitHubProfileInfos()
